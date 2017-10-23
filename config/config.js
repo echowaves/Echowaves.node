68481d548e7fb7e6a2b7599ba910e73a64cfff81
dmitryame <dmitry@custommode.com>
2017-10-22 22:00:53 -0400

    changed the db timeout

--- config/config.js
+++ config/config.js
@@ -8,20 +8,23 @@ module.exports = {
     password: process.env.EW_DB_PASS,
     database: process.env.EW_DB_NAME,
     host: process.env.EW_DB_HOST,
-    dialect: 'postgres'
+    dialect: 'postgres',
+    timeout: 100000
   },
   test: {
     username: process.env.EW_DB_USER,
     password: process.env.EW_DB_PASS,
     database: process.env.EW_DB_NAME,
     host: process.env.EW_DB_HOST,
-    dialect: 'postgres'
+    dialect: 'postgres',
+    timeout: 100000
   },
   production: {
     username: process.env.EW_DB_USER,
     password: process.env.EW_DB_PASS,
     database: process.env.EW_DB_NAME,
     host: process.env.EW_DB_HOST,
-    dialect: 'postgres'
+    dialect: 'postgres',
+    timeout: 100000
   }
 }

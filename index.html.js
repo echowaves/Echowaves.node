157b645d6044a383d20bf42ae967357b68d6c83f
dmitryame <dmitry@custommode.com>
2017-10-22 23:31:47 -0400

    fixing bundle

--- index.html.js
+++ index.html.js
@@ -2,16 +2,16 @@
 
 export default (title =>
   `
+  <!DOCTYPE html>
   <html>
     <head>
       <title>${title}</title>
-      <base href="">
       <link rel="stylesheet" href="/style/style.css">
         <link rel="stylesheet" href="https://cdn.rawgit.com/twbs/bootstrap/v4-dev/dist/css/bootstrap.css">
         </head>
         <body>
           <div class="container"></div>
-          <script type="text/javascript" src="bundle.js"></script>
+          <script type="text/javascript" src="/bundle.js"></script>
         </body>
       </html>
       `

fa7a1c69d9e9c88aac81ebc96b9f426bed22ba53
dmitryame <dmitry@custommode.com>
2017-12-02 17:44:38 -0500

    debubg

--- api/src/controllers/photo.js
+++ api/src/controllers/photo.js
@@ -190,7 +190,7 @@ exports.addPhoto = async ctx => {
     try {
         await sequelize.query('DELETE FROM \"AbuseReports\" where \"createdAt\" < NOW() - INTERVAL \'7 days\'')
         rowids = await sequelize.query('select id from (select id from \"Photos\" order by id desc  limit 100) as r order by id')
-        results = await sequelize.query('DELETE FROM \"Photos\" where \"createdAt\" < NOW() - INTERVAL \'24 hours\'')// and id < ' + rowids[0])
+        results = await sequelize.query('DELETE FROM \"Photos\" where \"createdAt\" < NOW() - INTERVAL \'24 hours\'')// and id < ' + rowids[0].id)
 
     } catch(err) {
       logger.error("Unable to cleanup Photos", err)
@@ -203,5 +203,5 @@ exports.addPhoto = async ctx => {
 
     // Resond to request indicating the photo was created
     ctx.response.status = 200
-    ctx.body = { status: 'success', results, rowid: rowids[0]}
+    ctx.body = { status: 'success', results, rowids}
   }

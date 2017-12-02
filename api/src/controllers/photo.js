54ccf8de1669238a44082753feccc2a67decb754
dmitryame <dmitry@custommode.com>
2017-12-02 17:33:51 -0500

    roll back

--- api/src/controllers/photo.js
+++ api/src/controllers/photo.js
@@ -195,7 +195,7 @@ exports.addPhoto = async ctx => {
     } catch(err) {
       logger.error("Unable to cleanup Photos", err)
       ctx.response.status = 500
-      ctx.body = { error: 'Unable to cleanup Photos', rowids}
+      ctx.body = { error: 'Unable to cleanup Photos'}
       return
     }
 

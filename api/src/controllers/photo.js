174cfafb7de55f738a4bd9c006e86b710c7dc3e8
dmitryame <dmitry@custommode.com>
2017-12-06 20:41:46 -0500

    trying to return binary data

--- api/src/controllers/photo.js
+++ api/src/controllers/photo.js
@@ -185,8 +185,10 @@ exports.addPhoto = async ctx => {
     // Resond to request indicating the photo was created
     // ctx.response.status = 200
     // ctx.response.type = "image/png"
-    ctx.body = new Buffer(photo.thumbNail.data)
+    // ctx.body = new Buffer(photo.thumbNail.data)
     // ctx.body = { data: [...photo.thumbNail.data] }
+    ctx.body = { error: 'not 2 found' }
+
   }
 
 
